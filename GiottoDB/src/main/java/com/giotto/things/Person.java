c2f429a4c826f0499e7246cda97f8c8b7243edf3
Joonho Park <joonho.p@gmail.com>
2016-07-20 02:08:20 -0400

    update2

--- GiottoDB/src/main/java/com/giotto/things/Person.java
+++ GiottoDB/src/main/java/com/giotto/things/Person.java
@@ -1,9 +1,21 @@
 package com.giotto.things;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.mongodb.util.JSON;
+
 public class Person implements Thing {
 	private String _id;
 	public String location;
 	public String name;
+	public JSON other;
 	
 	public String getName() {
 		// TODO Auto-generated method stub
@@ -16,7 +28,24 @@ public class Person implements Thing {
 	
 	@Override
 	public String toString(){
-		return "Name :" + name + ", location : " + location;
+		HashMap<String,Object> result;
+		System.out.println(other);
+		try {
+			result = new ObjectMapper().readValue(other.toString(), HashMap.class);
+
+			System.out.println(result);
+			return result.toString();
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
--- GiottoDB/src/main/java/com/giotto/web/Resource.java
+++ GiottoDB/src/main/java/com/giotto/web/Resource.java
@@ -1,6 +1,8 @@
 package com.giotto.web;
 
+import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -9,6 +11,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import com.giotto.db.DBManager;
 import com.giotto.things.Person;
@@ -28,9 +34,14 @@ public class Resource {
   @POST
   @Path("/find")
   @Consumes({"application/json"})
-  public String getPerson(Person name) throws UnknownHostException {
-	  System.out.println(name.toString());
-	  return DBManager.query("People", "matthew");	  
+  public String getPerson(String name) throws JsonParseException, JsonMappingException, IOException {
+	  System.out.println(name);
+	  HashMap result = new ObjectMapper().readValue(name, HashMap.class);
+	  HashMap other = (HashMap) (result.get("other"));
+	  System.out.println(other.get("HI"));
+	  
+//	  return DBManager.query("People", "matthew");
+	  return "";
   }
   
 }
